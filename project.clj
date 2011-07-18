0f430442d80b4bae8d4d4afb3e94238cf3b51866
Benjamin Teuber <bsteuber@gmail.com>
2011-07-18 16:15:17 +0200

    move project to contentjon

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.gostrategy/clj-go "1.0-SNAPSHOT"
+(defproject com.contentjon/clj-go "1.0-SNAPSHOT"
   :description "A general library for the asian game of Go"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]]
